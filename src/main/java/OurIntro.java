2fa8c428f281421a697d58fa97f25bf172568c1e
Makaij23 <70521051+Makaij23@users.noreply.github.com>
2020-09-17 23:25:03 -0400

    Initial commit

---
+++ src/main/java/OurIntro.java
@@ -0,0 +1,25 @@
+//each program must have a class
+//a class holds all the
+//   methods, variables, other statements
+//only one PUBLIC class per .java file
+//the public class name should match the
+//java file name
+public class OurIntro {
+    //each standalone application
+    //needs a main method to run
+    //execution start in the main
+    //and goes from top to bottom
+
+        //To display console messages to the
+        //user we use the "System" class
+        //the "System" class has an object
+        //called "out" (this will make more
+        //sense later)
+        //the "out" object can call a number
+        //of methods
+        //  print, println, printf
+        //we place the messages we want to
+        //display to the user between the
+        //() of the method we called
+
+}//end of class
